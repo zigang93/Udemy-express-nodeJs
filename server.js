fd41e751c02c06f944ca93a0ee070bd17f8bf0f5
zigang93 <tanzigang93@gmail.com>
2017-06-27 18:13:25 +0800

    start to deploy to heroku 20170627

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
+
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -58,6 +60,6 @@ app.get('/bad', (req , res) => {
 });
 
 
-app.listen(3000, () => {
-    console.log("Server is up..")
+app.listen(port, () => {
+    console.log(`Server is up..on ${port}`)
 });
