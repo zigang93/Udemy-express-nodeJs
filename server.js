2c643852c0b7df086b9f8a553f78af177a0fae1b
zigang93 <tanzigang93@gmail.com>
2017-06-27 17:39:10 +0800

    learning node.js express 20170627

---
+++ server.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view_engine', 'hbs');
+
+
+//check server log error
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`
+    console.log(log);
+    fs.appendFile('server.log', log + '\n' , (err) => {
+        if (err) {
+            console.log('Unable to append to server.log.')
+        }
+    });
+    next();
+});
+
+//without next , will force user back to maintenance page.
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear' , () =>{
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt' , (text) => {
+    return text.toUpperCase();
+});
+
+app.get('/', (req , res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        WelcomeMsg: 'Welcome to my express hbs templete',
+        currentYear: new Date().getFullYear()
+    });
+});
+
+app.get('/about', (req , res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page', 
+        currentYear: new Date().getFullYear()
+    });
+});
+
+app.get('/bad', (req , res) => {
+    res.send({
+        error: 'Unable to handle the request.. error'
+    })
+});
+
+
+app.listen(3000, () => {
+    console.log("Server is up..")
+});
